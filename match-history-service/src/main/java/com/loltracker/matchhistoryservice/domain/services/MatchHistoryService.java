ac6d90b81b8de0a341064f665b35672a23eaa03f
ivan280809 <ivanrobertomaneroii7@gmail.com>
2025-05-20 22:12:27 +0200

    refactor MatchHistoryService; improve code readability by restructuring method calls and enhancing stream operations for match filtering

--- match-history-service/src/main/java/com/loltracker/matchhistoryservice/domain/services/MatchHistoryService.java
+++ match-history-service/src/main/java/com/loltracker/matchhistoryservice/domain/services/MatchHistoryService.java
@@ -5,12 +5,10 @@ import com.loltracker.matchhistoryservice.domain.mappers.AccountMatchesMapper;
 import com.loltracker.matchhistoryservice.infrastructure.models.AccountMatchesMO;
 import com.loltracker.matchhistoryservice.infrastructure.models.MatchMO;
 import com.loltracker.matchhistoryservice.infrastructure.repositories.AccountMatchesRepository;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import lombok.val;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -31,33 +29,41 @@ public class MatchHistoryService {
     String puuid = matches.getAccountDTO().getPuuid();
     List<MatchMO> incomingMatches = mapToMatchMos(matches);
 
-    AccountMatchesMO entity = accountMatchesRepository.findById(puuid)
-            .map(existing -> updateExisting(existing, incomingMatches, matches.getAccountDTO().getGameName()))
+    AccountMatchesMO entity =
+        accountMatchesRepository
+            .findById(puuid)
+            .map(
+                existing ->
+                    updateExisting(
+                        existing, incomingMatches, matches.getAccountDTO().getGameName()))
             .orElseGet(() -> createNew(matches));
 
     accountMatchesRepository.save(entity);
   }
 
   private List<MatchMO> mapToMatchMos(AccountMatchesDTO matches) {
-    return matches.getMatchesDTO().getMatches()
-            .stream()
-            .map(AccountMatchesMapper::toMatchMO)
-            .toList();
+    return matches.getMatchesDTO().getMatches().stream()
+        .map(AccountMatchesMapper::toMatchMO)
+        .toList();
   }
 
-  private AccountMatchesMO updateExisting(AccountMatchesMO existing,
-                                        List<MatchMO> incomingMatches,
-                                        String gameName) {
+  private AccountMatchesMO updateExisting(
+      AccountMatchesMO existing, List<MatchMO> incomingMatches, String gameName) {
 
-    List<MatchMO> newMatches = incomingMatches.stream().filter(
-            newMatch -> existing.getMatchesMO().getMatches()
-                    .stream()
-                    .noneMatch(existingMatch -> existingMatch.getMetadata().getMatchId().equals(newMatch.getMetadata().getMatchId()))
-    ).toList();
+    List<MatchMO> newMatches =
+        incomingMatches.stream()
+            .filter(
+                newMatch ->
+                    existing.getMatchesMO().getMatches().stream()
+                        .noneMatch(
+                            existingMatch ->
+                                existingMatch
+                                    .getMetadata()
+                                    .getMatchId()
+                                    .equals(newMatch.getMetadata().getMatchId())))
+            .toList();
     // TODO: create listener with matchAnalyzerService
-    existing.getMatchesMO()
-            .getMatches()
-            .addAll(newMatches);
+    existing.getMatchesMO().getMatches().addAll(newMatches);
     log.info("Player {} matches updated in the database", gameName);
     return existing;
   }
