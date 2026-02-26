ef0c69f10df26f64d870f9dc2f074c91a44df538
Ivan <ivanrobertomaneroii7@gmail.com>
2026-02-27 00:04:45 +0100

    chore: refactor structure

--- src/main/java/com/loltracker/lolmatchtracker/LolMatchTrackerApplication.java
+++ src/main/java/com/loltracker/lolmatchtracker/LolMatchTrackerApplication.java
@@ -2,8 +2,12 @@ package com.loltracker.lolmatchtracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.loltracker")
+@EnableJpaRepositories(basePackages = "com.loltracker")
+@EntityScan(basePackages = "com.loltracker")
 public class LolMatchTrackerApplication {
 
   public static void main(String[] args) {
